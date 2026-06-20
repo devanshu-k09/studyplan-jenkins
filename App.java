fb87839793f3848972ae7ccdf150cc9d45647cce
Devanshu <devanshu@gmail.com>
2026-06-20 18:55:58 +0530

    feat: upgrade to spring boot web server on port 8000

--- App.java
+++ App.java
@@ -1,8 +1,22 @@
 package com.studyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Build is Working!");
-        System.out.println("Hello, Jenkins Build is Working automatically via Webhook!");
-        System.out.println("Jenkins Auto Deploy Test");
+        // This line keeps the application running continuously
+        SpringApplication.run(App.class, args);
+    }
+
+    // This creates a web page path at http://your-ip/
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Hey Devanshu..! Your Jenkins CI/CD Java Automation Pipeline is Fully Working!</h1>";
     }
 }
