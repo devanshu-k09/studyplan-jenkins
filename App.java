05f7e637fb8e86b3fb9fe5e328a3d307fafae39b
Devanshu <devanshu@gmail.com>
2026-06-20 18:26:55 +0530

    Testing Jenkins Deploy

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Build is Working!");
         System.out.println("Hello, Jenkins Build is Working automatically via Webhook!");
+        System.out.println("Jenkins Auto Deploy Test");
     }
 }
