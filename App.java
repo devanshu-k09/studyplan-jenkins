68a317868260ed68f9ca396985dcba701fd4b684
Devanshu <devanshu@gmail.com>
2026-06-20 18:12:47 +0530

    test: triggering automated webhook build

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ package com.studyplanner;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins Build is Working!");
+        System.out.println("Hello, Jenkins Build is Working automatically via Webhook!");
     }
 }
